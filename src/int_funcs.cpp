935abc0b47d18b0c93967972dd88557445ba1a53
Yuchen Wang <ycwang0712@gmail.com>
2015-10-31 14:49:48 -0400

    init

---
+++ src/int_funcs.cpp
@@ -0,0 +1,24 @@
+#include <Rcpp.h>
+#include <mvtnormAPI.h>
+
+using namespace Rcpp;
+
+// A self-contained function that only include necessary headers.
+// Internal function, doesn't need to export to other packages.
+
+// [[Rcpp::export]]
+double pmvnorm(NumericVector lls, NumericVector uls,
+               NumericVector mu, NumericMatrix sigma) {
+
+  int n = 2, nu = 0, maxpts = 2000, inform;
+  int infin [2] = {2, 2};
+  double abseps = 1/1000, releps = 1/1000, error, value;
+  int rnd = 1;
+
+  double corr = sigma(0, 1) / sqrt(sigma(0, 0) * sigma(1, 1));
+
+  /* mvtnorm_C_mvtdst is defined in mvtnorm/inst/include/mvtnormAPI.h */
+  mvtnorm_C_mvtdst(&n, &nu, lls.begin(), uls.begin(), infin, &corr, mu.begin(),
+                   &maxpts, &abseps, &releps, &error, &value, &inform, &rnd);
+  return value;
+}
